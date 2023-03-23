a6ef7344c29a5da52e419dbd485bf57a7d23a610
kakosergio <sergioricardort@hotmail.com>
2023-03-23 06:40:53 -0300

    Config envirorment variables

---
+++ src/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	StringConnDB = ""
+	Port         = ""
+)
+
+// Carregar vai inicializar as variáveis de ambiente
+func Carregar() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	Port = os.Getenv("API_PORT")
+
+	StringConnDB = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("API_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PWD"),
+		os.Getenv("DB_NAME"),
+	)
+}
