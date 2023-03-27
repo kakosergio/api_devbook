ff1159551fb0976745d25da6310484aa565c03f8
kakosergio <sergioricardort@hotmail.com>
2023-03-27 06:43:49 -0300

    Going for responses architeture model

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -4,10 +4,9 @@ import (
 	"api/src/database"
 	"api/src/models"
 	repositories "api/src/repositories/user"
+	"api/src/responses"
 	"encoding/json"
-	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
@@ -16,24 +15,29 @@ func CreateUser(w http.ResponseWriter, r *http.Request){
 	requestBody, err := io.ReadAll(r.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 	var user models.User
 	if err = json.Unmarshal(requestBody, &user); err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusBadRequest, err)
+		return
 	}
 
 	db, err := database.Connect()
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
+	defer db.Close()
 
 	repository := repositories.UsersRepository(db)
-	id, err := repository.Create(user)
+	user.Id, err = repository.Create(user)
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
-	w.Write([]byte(fmt.Sprintf("Usuário inserido. ID: %d", id)))
+	responses.JSON(w, http.StatusCreated, user)
 }
 
 // FindAllUsers busca no banco de dados todos os usuários cadastrados
