c6de60ddd4e48f9647bc11da0f724a22139087b2
kakosergio <sergioricardort@hotmail.com>
2023-04-14 06:43:08 -0300

    FindFollowing

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -290,3 +290,29 @@ func FindFollowers (w http.ResponseWriter, r *http.Request){
 
 	responses.JSON(w, http.StatusOK, followers)
 }
+
+func FindFollowing (w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	userID, err := strconv.ParseUint(params["id"], 10, 64)
+
+	if err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+	defer db.Close()
+
+	repository := repositories.UsersRepository(db)
+	following, err := repository.FindFollowing(userID)
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, following)
+}
--- src/repositories/users.go
+++ src/repositories/users.go
@@ -191,3 +191,32 @@ func (repository user) FindFollowers (userID uint64) ([]models.User, error){
 	}
 	return users, nil
 }
+
+// FindFollowing busca todos os seguidores de determinado usuário
+func (repository user) FindFollowing (userID uint64) ([]models.User, error){
+	// Selecionar id, nome, nick, email, criadoEm da tabela de usuários em combinação com a tabela de seguidores onde userID é igual a followerID
+	rows, err := repository.db.Query(`SELECT u.id, u.name, u.nick, u.email, u.createdOn 
+	FROM users u INNER JOIN followers s ON u.id = s.user_id WHERE s.follower_id = $1`, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var users []models.User
+	for rows.Next(){
+		var user models.User
+
+		if err = rows.Scan(
+			&user.Id,
+			&user.Name,
+			&user.Nick,
+			&user.Email,
+			&user.CreatedOn,
+		); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+	return users, nil
+}
