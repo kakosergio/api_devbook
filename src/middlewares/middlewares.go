d029e204f176f4e3c6df819d71247d935d47035e
kakosergio <sergioricardort@hotmail.com>
2023-04-21 18:36:32 -0300

    Some comments

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -14,7 +14,7 @@ type statusRecorder struct {
 	statusCode int
 }
 
-// WriteHeader é uma função auxiliar que grava o statuscode no header do responsewriter. Ela implementa a interface ResponseWriter
+// WriteHeader é uma função auxiliar que grava o statuscode no header do responsewriter. Ela implementa a interface http.ResponseWriter
 // Isso significa que será chamada toda vez que o responseWriter do struct statusRecorder for solicitado, no lugar do WriteHeader do handler original.
 func (recorder *statusRecorder) WriteHeader (code int){
 	recorder.statusCode = code
@@ -24,9 +24,12 @@ func (recorder *statusRecorder) WriteHeader (code int){
 // Logger escreve informações da requisição no terminal
 func Logger (next http.HandlerFunc) http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request){
+		// Cria um statusCode padrão para caso o WriteHeader não for chamado.
 		rec := statusRecorder{w, 200}
 
+		// Passa o controle para o próximo handler
 		next.ServeHTTP(&rec, r)
+		// Printa na tela o log com as informaçõe obtidas da request e do writer
 		log.Printf("%s%s [%d] %s", r.Host, r.URL.Path, rec.statusCode, r.Method)
 	}
 }
