1eda65831bea1000e971f91047f11e891df11920
kakosergio <sergioricardort@hotmail.com>
2023-04-21 18:34:07 -0300

    Created statusCode recorder for middleware Logger

--- src/middlewares/middlewares.go
+++ src/middlewares/middlewares.go
@@ -5,14 +5,29 @@ import (
 	"api/src/responses"
 	"log"
 	"net/http"
-	"time"
+
 )
+
+// statusRecorder cria um struct que vai guardar as informações do responsewriter para passar para o próximo handler e gravar o statuscode
+type statusRecorder struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// WriteHeader é uma função auxiliar que grava o statuscode no header do responsewriter. Ela implementa a interface ResponseWriter
+// Isso significa que será chamada toda vez que o responseWriter do struct statusRecorder for solicitado, no lugar do WriteHeader do handler original.
+func (recorder *statusRecorder) WriteHeader (code int){
+	recorder.statusCode = code
+	recorder.ResponseWriter.WriteHeader(code)
+}
 
 // Logger escreve informações da requisição no terminal
 func Logger (next http.HandlerFunc) http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request){
-		log.Printf("\n%s %s [%s] %s", time.Now(), r.Host, r.Method, r.URL.Path)
-		next(w, r)
+		rec := statusRecorder{w, 200}
+
+		next.ServeHTTP(&rec, r)
+		log.Printf("%s%s [%d] %s", r.Host, r.URL.Path, rec.statusCode, r.Method)
 	}
 }
 
